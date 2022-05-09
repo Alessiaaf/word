127b42d6341b7586d9a4f9ab8131224408dfdfca
alessia <alessia@gmail.com>
2022-05-09 17:50:07 +0200

    primo commit

---
+++ class Stars {.java
@@ -0,0 +1,10 @@
+class Stars {
+	public static void main (String [] args ) {
+		int n = Integer . parseInt (args [0]);
+		for (int j = 0; i=n; i++ ) {
+			for (int  j = 0; j<i+1; j ++)
+				System . out . print ("*");
+			Sistem . out . printIn ();
+		}
+	}
+}
