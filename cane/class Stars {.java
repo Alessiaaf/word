8433d47d07969f0c61a51f832abd3f0fa021b405
alessia <alessia@gmail.com>
2022-05-09 18:22:15 +0200

    mod file

--- cane/class Stars {.java
+++ cane/class Stars {.java
@@ -2,7 +2,7 @@ class Stars {
 	public static void main (String [] args ) {
 		int n = Integer . parseInt (args [0]);
 		for (int j = 0; i=n; i++ ) {
-			for (int  j = 0; j<i+1; j ++)
+			for (  j = 0; j<i+1; j ++)
 				System . out . print ("*");
 			Sistem . out . printIn ();
 		}
